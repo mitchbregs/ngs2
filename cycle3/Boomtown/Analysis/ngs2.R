1d5f9fd37b886f29712da1358f4cda9e0c968979
Mitchell Bregman <mitchbregs@gmail.com>
2020-06-23 16:34:31 -0400

    rcurl test43

--- cycle3/Boomtown/Analysis/ngs2.R
+++ cycle3/Boomtown/Analysis/ngs2.R
@@ -1,2 +1,2 @@
 library(rmarkdown)
-render('NGS2_WITNESS_Cycle3_TEST.rmd', output_file='output/NGS2_WITNESS_Cycle3.html')
+render('NGS2_WITNESS_Cycle3_h15_TOY.rmd', output_file='output/NGS2_WITNESS_Cycle3_h15_TOY.html')
